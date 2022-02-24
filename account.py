388cf455312ab0f044813fed19d5b3b7ac7fab9c
Diego <diego.oroza80@gmail.com>
2022-02-24 13:29:47 -0600

    Instantiating classes

--- account.py
+++ account.py
@@ -1,7 +1,3 @@
-from unicodedata import name
-from xml.dom.minidom import Document
-
-
 class Account():
     id          =  int
     name        = str
--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from car import Car
+
 if __name__== '__main__':
-    print('Hello World!')
+    #print('Hello World!')
+    car1=Car()
+    car1.id=4512
+    car1.license='AMS234'
+    car1.driver='Juan Perez'
+    car1.passenger='Manuel Ramos'
+    print(vars(car1))
+
+    car2=Car()
+    car2.id=4513
+    car2.license='AMS237'
+    car2driver='Juana Laoz'
+    car2.passenger='Manny Jones'
+    print(vars(car2))
