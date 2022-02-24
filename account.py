e489571e78bb6f31937ba6ee5cfec4f0bf335283
Diego <diego.oroza80@gmail.com>
2022-02-23 22:08:02 -0600

    Creating classes

---
+++ account.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from xml.dom.minidom import Document
+
+
+class Account():
+    id          =  int
+    name        = str
+    document    =    str
+    email       =   str
+    password    = str
+    
+
